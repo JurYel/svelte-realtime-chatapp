9f499ccf41d535a7b63860cb896a9adff63b2b05
Juriel Botoy <juriel.botoy182@gmail.com>
2022-05-04 01:02:29 +0800

    [SVECHT-001] - Setting up Google Authentication

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
 <script>
-	
+	import {auth, googleProvider} from "./firebase";
+	import {authState} from "rxfire/auth";
+
+	let user;
+
+	const unsubscribe = authState(auth).subscribe(usr => user = usr);
+
+	function login(){
+		auth.signInWithPopup(googleProvider);
+	}
+
+	function logout() {
+		auth.signout();
+	}
 </script>
 
 
@@ -8,5 +21,12 @@
 </style>
 
 <main>
-	Hello world!
+	{#if user}
+		Logged in as {user.uid}
+	{:else}
+		<button on:click={login}>
+			<i class="fa fa-google"></i>
+			Sign In with Google
+		</button>
+	{/if}
 </main>
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDq9M7aQmo0wwtw-KiXRMys5tpY4o7kHSE",
+    authDomain: "svelte-realtime-chatapp.firebaseapp.com",
+    projectId: "svelte-realtime-chatapp",
+    storageBucket: "svelte-realtime-chatapp.appspot.com",
+    messagingSenderId: "128871405600",
+    appId: "1:128871405600:web:9dd6df1ee76d9526674445"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const db = firebase.firestore();
+
