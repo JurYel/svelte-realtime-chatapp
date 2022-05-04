9feb9bb2971478fd00dae10cbb8c1415ffb0885e
Juriel Botoy <juriel.botoy182@gmail.com>
2022-05-04 21:23:00 +0800

    [SVECHT-005] - Finishing touches

--- src/Chatroom.svelte
+++ src/Chatroom.svelte
@@ -29,7 +29,114 @@
 </script>
 
 <style>
+    main {
+        height: 100%;
+    }
 
+    /* Chat Header */
+    .header {
+        width: 100%;
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0px 10px;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .header .logo{
+        font-size: 15px;
+        font-weight: 600;
+        color: #111;
+    }
+
+    .header button {
+        background: transparent;
+        padding: 5px 10px;
+        border: 1px solid #ddd;
+        color: #555;
+        font-size: 15px;
+        cursor: pointer;
+    }
+
+    .form {
+        display: flex;
+        width: 100%;
+        height: 40px;
+        border-top: 1px solid #ddd;
+    }
+
+    .form input {
+        flex: 1;
+        border: none;
+        outline: none;
+        font-size: 16px;
+        color: #111;
+        padding: 10px;
+    }
+
+    .form button {
+        padding: 10px;
+        font-size: 20px;
+        color: #555;
+        background: transparent;
+        outline: none;
+        border: none;
+        cursor: pointer;
+    }
+
+    .messages {
+        width: 100%;
+        height: calc(100% - 90px);
+        padding: 10px;
+        overflow-y: auto;
+    }
+
+    .messages::-webkit-scrollbar {
+        width: 4px;
+        background: #fff;
+    }
+
+    .message::-webkit-scrollbar-thumb{
+        width: 4px;
+        background: #888;
+    }
+
+    .messages .message {
+        margin: 10px 0px; 
+        display: flex;
+    }
+
+    .messages .my-message{
+        justify-content: flex-end;
+    }
+
+    .messages .message .avatar {
+        width: 30px;
+        height: 30px;
+        margin-right: 8px;
+    }
+
+    .messages .message .text {
+        padding: 10px;
+        background: #f0f4f7;
+        color: #555;
+        font-size: 13px;
+        font-weight: 600;
+        max-width: fit-content;
+        flex: 1;
+        border-radius: 5px;
+    }
+
+    .messages .message .avatar img{
+        width: 100%;
+        height: 100%;
+    }
+
+    .message .my-message .text{
+        background: #555;
+        color: #fff;
+    }
 </style>
 
 <main>
